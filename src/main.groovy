261c866e784e6823e203c34ca371f7eddb9d8b06
xnat <xnatural@msn.cn>
2021-08-17 17:19:04 +0800

    pref: pretty code

--- src/main.groovy
+++ src/main.groovy
@@ -6,7 +6,6 @@ import cn.xnatural.enet.event.EP
 import cn.xnatural.jpa.Repo
 import cn.xnatural.remoter.Remoter
 import cn.xnatural.sched.Sched
-
 import core.HttpSrv
 import core.OkHttpSrv
 import core.RedisClient
@@ -21,77 +20,27 @@ import service.rule.*
 
 import java.time.Duration
 
+
 System.setProperty("configdir", "../conf")
 @Field final Logger log = LoggerFactory.getLogger("ROOT")
 @Field final AppContext app = new AppContext() //应用上下文
-@Lazy @Field EP ep = app.bean(EP, null)
+// @Lazy @Field EP ep = app.bean(EP, null)
 
 
 // 系统功能添加区
-app.addSource(new OkHttpSrv()) // http 客户端
-app.addSource(new CacheSrv()) // 对象缓存
-app.addSource(new ServerTpl("sched") { // 定时任务
-    Sched sched
-    @EL(name = "sys.starting", async = true)
-    void start() {
-        sched = new Sched(attrs(), exec()).init()
-        exposeBean(sched)
-        ep.fire("${name}.started")
-    }
-
-    @EL(name = "sched.after")
-    void after(Duration duration, Runnable fn) {sched.after(duration, fn)}
-
-    @EL(name = "sys.stopping", async = true, order = 1f)
-    void stop() { sched?.stop() }
-})
-app.addSource(new ServerTpl("remoter") { // 集群配置
-    Remoter remoter
-    @EL(name = "sched.started")
-    void start() {
-        remoter = new Remoter(app.name(), app.id(), attrs(), exec(), ep, bean(Sched))
-        exposeBean(remoter)
-        exposeBean(remoter.aioClient)
-        ep.fire("${name}.started")
-    }
-
-    @EL(name = 'sys.heartbeat', async = true)
-    void heartbeat() {
-        remoter.sync()
-        remoter.aioServer.clean()
-    }
-    @EL(name = "sys.stopping", async = true)
-    void stop() { remoter.stop() }
-}) // 集群分布式
-app.addSource(new ServerTpl("jpa_rule") { //数据库 jpa_rule
-    Repo repo
-    @EL(name = "sys.starting", async = true)
-    void start() {
-        repo = new Repo(attrs()).entities( // jpa封装
-            Decision, RuleField, DataCollector, OpHistory, DecideRecord, CollectRecord,
-            User, Permission, GlobalConfig, Lock, UserSession
-        ).init()
-        exposeBean(repo, name + "_repo")
-        ep.fire("${name}.started")
-    }
-
-    // 数据库 最后再关闭
-    @EL(name = "sys.stopping", async = true, order = 2f)
-    void stop() { repo?.close() }
-})
-app.addSource(new HttpSrv().ctrls(
-        TestCtrl, MainCtrl, RuleCtrl, MntCtrl, MntUserCtrl, MntDecisionCtrl, MntAnalyseCtrl
-))
-app.addSource(new FileUploader())
-app.addSource(new CollectorManager())
-app.addSource(new TestService())
-app.addSource(new FieldManager())
-app.addSource(new DecisionSrv())
-app.addSource(new DecisionManager())
-app.addSource(new OpHistorySrv())
-app.addSource(new UserSrv())
-app.addSource(this)
-app.start() // 启动系统
+app.addSource(
+        new OkHttpSrv(), // http 客户端
+        new CacheSrv(), // 对象缓存
+        sched(), // 时间任务调度管理
+        remoter(), // 集群分布式
+        jpa_rule(), // 数据源rule
+        new HttpSrv().ctrls( // 接口层
+                TestCtrl, MainCtrl, RuleCtrl, MntCtrl, MntUserCtrl, MntDecisionCtrl, MntAnalyseCtrl
+        ),
+        new FileUploader(), new CollectorManager(), new TestService(),
+        new FieldManager(), new DecisionSrv(), new DecisionManager(),
+        new OpHistorySrv(), new UserSrv(), this
+).start() // 启动系统
 
 
 @EL(name = 'sys.inited')
@@ -107,6 +56,67 @@ void sysStarted() {
     try {
     } finally {
         // System.exit(0)
+    }
+}
+
+
+def jpa_rule() {
+    new ServerTpl("jpa_rule") { //数据库 jpa_rule
+        Repo repo
+        @EL(name = "sys.starting", async = true)
+        void start() {
+            repo = new Repo(attrs()).entities( // jpa封装
+                    Decision, RuleField, DataCollector, OpHistory, DecideRecord, CollectRecord,
+                    User, Permission, GlobalConfig, Lock, UserSession
+            ).init()
+            exposeBean(repo, name + "_repo")
+            ep.fire("${name}.started")
+        }
+
+        // 数据库 最后再关闭
+        @EL(name = "sys.stopping", async = true, order = 2f)
+        void stop() { repo?.close() }
+    }
+}
+
+
+def sched() {
+    new ServerTpl("sched") { // 定时任务
+        Sched sched
+        @EL(name = "sys.starting", async = true)
+        void start() {
+            sched = new Sched(attrs(), exec()).init()
+            exposeBean(sched)
+            ep.fire("${name}.started")
+        }
+
+        @EL(name = "sched.after")
+        void after(Duration duration, Runnable fn) {sched.after(duration, fn)}
+
+        @EL(name = "sys.stopping", async = true, order = 1f)
+        void stop() { sched?.stop() }
+    }
+}
+
+
+def remoter() {
+    new ServerTpl("remoter") { // 集群配置
+        Remoter remoter
+        @EL(name = "sched.started")
+        void start() {
+            remoter = new Remoter(app.name(), app.id(), attrs(), exec(), ep, bean(Sched))
+            exposeBean(remoter)
+            exposeBean(remoter.aioClient)
+            ep.fire("${name}.started")
+        }
+
+        @EL(name = 'sys.heartbeat', async = true)
+        void heartbeat() {
+            remoter.sync()
+            remoter.aioServer.clean()
+        }
+        @EL(name = "sys.stopping", async = true)
+        void stop() { remoter.stop() }
     }
 }
 
