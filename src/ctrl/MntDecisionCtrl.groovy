c09af539a10d82ac0146162c82ef42b5a4220253
xnat <xnatural@msn.cn>
2021-10-29 13:33:48 +0800

    opt: delete collector useless code

--- src/ctrl/MntDecisionCtrl.groovy
+++ src/ctrl/MntDecisionCtrl.groovy
@@ -104,28 +104,6 @@ class MntDecisionCtrl extends ServerTpl {
     }
 
 
-    @Path(path = 'dataCollectorPage')
-    ApiResp dataCollectorPage(HttpContext hCtx, Integer page, Integer pageSize, String kw, String id, String type) {
-        if (pageSize && pageSize > 50) return ApiResp.fail("Param pageSize <=50")
-        hCtx.auth("dataCollector-read")
-        ApiResp.ok(
-            repo.findPage(DataCollector, page, pageSize?:10) { root, query, cb ->
-                query.orderBy(cb.desc(root.get('updateTime')))
-                def ps = []
-                if (kw) {
-                    ps << cb.or(
-                        cb.like(root.get('name'), '%' + kw + '%'),
-                        cb.like(root.get('comment'), '%' + kw + '%')
-                    )
-                }
-                if (id) ps << cb.equal(root.get("id"), id)
-                if (type) ps << cb.equal(root.get('type'), type)
-                ps ? ps.inject {p1, p2 -> cb.and(p1, p2)} : null
-            }
-        )
-    }
-
-
     @Path(path = 'opHistoryPage')
     ApiResp opHistoryPage(HttpContext hCtx, Integer page, Integer pageSize, String kw, String type, String startTime, String endTime) {
         if (pageSize && pageSize > 20) return ApiResp.fail("Param pageSize <=20")
@@ -456,69 +434,6 @@ class MntDecisionCtrl extends ServerTpl {
     }
 
 
-    @Path(path = 'addDataCollector', method = 'post')
-    ApiResp addDataCollector(
-        HttpContext hCtx, String name, String type, String url, String bodyStr, Boolean recordResult,
-        String method, String parseScript, String contentType, String comment, String computeScript, String dataSuccessScript,
-        String sqlScript, Integer minIdle, Integer maxActive, Integer timeout, Boolean enabled, String cacheKey, String cacheTimeoutFn
-    ) {
-        hCtx.auth('dataCollector-add')
-        DataCollector collector = new DataCollector(name: name, type: type, comment: comment, enabled: (enabled == null ? true : enabled), recordResult: recordResult == null ? true : recordResult)
-        if (!collector.name) return ApiResp.fail('Param name not empty')
-        if (!collector.type) return ApiResp.fail('Param type not empty')
-        if ('http' == collector.type) {
-            if (!url) return ApiResp.fail('Param url not empty')
-            if (!method) return ApiResp.fail('Param method not empty')
-            if (!contentType && !'get'.equalsIgnoreCase(method)) return ApiResp.fail('Param contentType not empty')
-            if (!url.startsWith("http") && !url.startsWith('${')) return ApiResp.fail('Param url incorrect')
-            collector.parseScript = parseScript?.trim()
-            if (collector.parseScript && !collector.parseScript.startsWith("{") && !collector.parseScript.endsWith("}")) {
-                return ApiResp.fail('Param parseScript a function, must startWith {, endWith }')
-            }
-            collector.dataSuccessScript = dataSuccessScript?.trim()
-            if (collector.dataSuccessScript && !collector.dataSuccessScript.startsWith("{") && !collector.dataSuccessScript.endsWith("}")) {
-                return ApiResp.fail('Param dataSuccessScript a function, must startWith {, endWith }')
-            }
-            collector.url = url
-            collector.method = method
-            collector.bodyStr = bodyStr
-            collector.contentType = contentType
-            collector.timeout = timeout
-        } else if ('script' == collector.type) {
-            if (!computeScript) return ApiResp.fail('Param computeScript not empty')
-            collector.computeScript = computeScript
-        } else if ('sql' == collector.type) {
-            if (!url) return ApiResp.fail('Param url not empty')
-            if (!sqlScript) return ApiResp.fail('Param sqlScript not empty')
-            if (!url.startsWith("jdbc")) return ApiResp.fail('url incorrect')
-            if (minIdle < 0 || collector.minIdle > 20) return ApiResp.fail('Param minIdle >=0 and <= 20')
-            if (maxActive < 1 || collector.maxActive > 50) ApiResp.fail('Param maxActive >=1 and <= 50')
-            collector.url = url
-            collector.sqlScript = sqlScript
-            collector.minIdle = minIdle
-            collector.maxActive = maxActive
-        } else return ApiResp.fail('Not support type: ' + collector.type)
-        collector.creator = hCtx.getSessionAttr("uName")
-        collector.cacheKey = cacheKey
-        collector.cacheTimeoutFn = cacheTimeoutFn
-        try {
-            repo.saveOrUpdate(collector)
-        } catch (ex) {
-            def cause = ex
-            while (cause != null) {
-                if (cause.message.contains("Duplicate entry")) {
-                    return ApiResp.fail("$name aleady exist")
-                }
-                cause = cause.cause
-            }
-            throw ex
-        }
-        ep.fire('dataCollectorChange', collector.id)
-        ep.fire('enHistory', collector, hCtx.getSessionAttr('uName'))
-        ApiResp.ok(collector)
-    }
-
-
     @Path(path = 'updateField', method = 'post')
     ApiResp updateField(HttpContext hCtx, Long id, String enName, String cnName, FieldType type, String comment, String collectorOptions) {
         hCtx.auth('field-update')
@@ -573,95 +488,6 @@ class MntDecisionCtrl extends ServerTpl {
     }
 
 
-    @Path(path = 'updateDataCollector', method = 'post')
-    ApiResp updateDataCollector(
-        HttpContext hCtx, String id, String name, String url, String bodyStr, Boolean recordResult,
-        String method, String parseScript, String contentType, String comment, String computeScript, String dataSuccessScript,
-        String sqlScript, Integer minIdle, Integer maxActive, Integer timeout, Boolean enabled, String cacheKey, String cacheTimeoutFn
-    ) {
-        hCtx.auth('dataCollector-update')
-        if (!id) return ApiResp.fail("Param id not required")
-        if (!name) return ApiResp.fail("Param name not empty")
-        def collector = repo.findById(DataCollector, id)
-        if (collector == null) return ApiResp.fail("Param id: $id not found")
-        if ('http' == collector.type) {
-            if (!url) return ApiResp.fail('Param url not empty')
-            if (!method) return ApiResp.fail('Param method not empty')
-            if ('post'.equalsIgnoreCase(method) && !contentType) {
-                return ApiResp.fail('Param contentType not empty')
-            }
-            if (!url.startsWith("http") && !url.startsWith('${')) return ApiResp.fail('Param url incorrect')
-            collector.parseScript = parseScript?.trim()
-            if (collector.parseScript && (!collector.parseScript.startsWith('{') || !collector.parseScript.endsWith('}'))) {
-                return ApiResp.fail('Param parseScript is function, must startWith {, endWith }')
-            }
-            collector.dataSuccessScript = dataSuccessScript?.trim()
-            if (collector.dataSuccessScript && !collector.dataSuccessScript.startsWith("{") && !collector.dataSuccessScript.endsWith("}")) {
-                return ApiResp.fail('Param dataSuccessScript is function, must startWith {, endWith }')
-            }
-            collector.url = url
-            collector.method = method
-            collector.contentType = contentType
-            collector.bodyStr = bodyStr
-            collector.timeout = timeout
-        } else if ('script' == collector.type) {
-            if (!computeScript) return ApiResp.fail('Param computeScript not empty')
-            collector.computeScript = computeScript?.trim()
-            if (collector.computeScript && (collector.computeScript.startsWith('{') || collector.computeScript.endsWith('}'))) {
-                return ApiResp.fail('Param computeScript is pure script. cannot startWith { or endWith }')
-            }
-        } else if ('sql' == collector.type) {
-            if (!url) return ApiResp.fail('Param url not empty')
-            if (!sqlScript) return ApiResp.fail('Param sqlScript not empty')
-            if (!url.startsWith("jdbc")) return ApiResp.fail('Param url incorrect')
-            if (minIdle < 0 || minIdle > 20) return ApiResp.fail('Param minIdle >=0 and <= 20')
-            if (maxActive < 1 || maxActive > 50) return ApiResp.fail('Param maxActive >=1 and <= 50')
-            collector.url = url
-            collector.minIdle = minIdle
-            collector.maxActive = maxActive
-            collector.sqlScript = sqlScript
-        }
-        collector.name = name
-        collector.comment = comment
-        collector.enabled = enabled == null ? true : enabled
-        collector.recordResult = recordResult == null ? true : recordResult
-        collector.cacheKey = cacheKey
-        collector.cacheTimeoutFn = cacheTimeoutFn
-        collector.updater = hCtx.getSessionAttr("uName")
-
-        try {
-            repo.saveOrUpdate(collector)
-        } catch (ex) {
-            def cause = ex
-            while (cause != null) {
-                if (cause.message.contains("Duplicate entry")) {
-                    return ApiResp.fail("$name aleady exist")
-                }
-                cause = cause.cause
-            }
-            throw ex
-        }
-        ep.fire('dataCollectorChange', collector.id)
-        ep.fire('enHistory', collector, hCtx.getSessionAttr('uName'))
-        ApiResp.ok(collector)
-    }
-
-
-    @Path(path = 'enableDataCollector', method = 'post')
-    ApiResp enableDataCollector(HttpContext hCtx, String id, Boolean enabled) {
-        hCtx.auth('dataCollector-update')
-        if (!id) return ApiResp.fail("Param id required")
-        if (enabled == null) return ApiResp.fail("Param enabled required")
-        def collector = repo.findById(DataCollector, id)
-        if (collector == null) return ApiResp.fail("Param id: $id not found")
-        collector.enabled = enabled
-        repo.saveOrUpdate(collector)
-        ep.fire('dataCollectorChange', collector.id)
-        ep.fire('enHistory', collector, hCtx.getSessionAttr('uName'))
-        ApiResp.ok(collector)
-    }
-
-
     @Path(path = 'delDecision/:id')
     ApiResp delDecision(HttpContext hCtx, String id) {
         if (!id) return ApiResp.fail("Param id required")
@@ -676,31 +502,13 @@ class MntDecisionCtrl extends ServerTpl {
 
     @Path(path = 'delField/:id')
     ApiResp delField(HttpContext hCtx, Long id) {
-        if (!id) return ApiResp.fail("Param id not empty")
+        if (!id) return ApiResp.fail("Param id required")
         hCtx.auth('field-del')
         def field = repo.findById(RuleField, id)
         repo.delete(field)
         ep.fire('fieldChange', id)
         ep.fire('enHistory', field, hCtx.getSessionAttr('uName'))
         ApiResp.ok()
-    }
-
-
-    @Path(path = 'delDataCollector/:id')
-    ApiResp delDataCollector(HttpContext hCtx, String id) {
-        if (!id) return ApiResp.fail("Param id not empty")
-        hCtx.auth('dataCollector-del')
-        def collector = repo.findById(DataCollector, id)
-        repo.delete(collector)
-        ep.fire('dataCollectorChange', id)
-        ep.fire('enHistory', collector, hCtx.getSessionAttr('uName'))
-        ApiResp.ok()
-    }
-
-
-    @Path(path = 'testCollector/:id')
-    ApiResp testCollector(String id, HttpContext hCtx) {
-        ApiResp.ok(collectorManager.testCollector(id, hCtx.params()))
     }
 
 
